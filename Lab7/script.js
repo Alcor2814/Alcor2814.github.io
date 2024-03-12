fcd0e87a82c4ab3843958cf1b59fb808d376636f
Cameron <cameron.smith2814@gmail.com>
2024-03-12 16:43:23 +0000

    Added Lab 7

---
+++ Lab7/script.js
@@ -0,0 +1,6 @@
+function helloFunction(){
+	alert('MURDER MURDER THERE HAS BEEN A MURDER');
+}
+function consoleFunction(){
+	console.log('MURDER MURDER THERE HAS BEEN A MURDER BUT ON THE CONSOLE');
+}
