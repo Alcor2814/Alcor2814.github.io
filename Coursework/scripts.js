1303d04a678d6deae08a40a4b89310592a5fbd57
Cameron <cameron.smith2814@gmail.com>
2024-04-14 17:04:06 +0100

    Continued developing features

--- Coursework/scripts.js
+++ Coursework/scripts.js
@@ -1,17 +1,25 @@
-var text1 = ["Apollo Mission", "Artemis Mission", "Voyager", "Mars Rovers", "Galaxies", "Comets", "Moon", "Stars", "Exoplanets"]
+var text1 = ["Apollo Missions", "Artemis Missions", "Voyagers", "Mars Rovers", "Galaxies", "Comets", "Moon", "Stars", "Exoplanets"]
+var text2 = ["Apollo", "Artemis", "Voyager", "Mars Rovers", "Galaxies", "Comets", "Moon", "Stars", "Exoplanets"]
+var folderName = ["Featured_Missions", "Featured_Missions", "Featured_Missions", "Featured_Missions", "Notable_Phenomena", "Notable_Phenomena", "Notable_Phenomena", "Notable_Phenomena", "Notable_Phenomena"]
 
-function randText(){
-	let id = getRndElement(0, text1.length);
-	document.getElementById('b1').innerHTML = text1[id];
-	text1.splice(id, 1);
+
+function randPage(){
 	
-	id = getRndElement(0, text1.length);
-	document.getElementById('b2').innerHTML = text1[id];
-	text1.splice(id, 1);
+	let id = 0;
+	let btn = "b1"
 	
-	id = getRndElement(0, text1.length);
-	document.getElementById('b3').innerHTML = text1[id];
-	text1.splice(id, 1);
+	for (let i = 1; i < 4; i++) {
+		id = getRndElement(0, text1.length);
+		btn = 'b'+i;
+		document.getElementById(btn).innerHTML = text1[id];
+		
+		var addr = folderName[id] + "/" + text2[id] + ".html";
+		document.getElementById(btn).setAttribute("href", addr);
+	
+		text1.splice(id, 1);
+		text2.splice(id, 1);
+		folderName.splice(id, 1);
+	} 
 };
 
 function getRndElement(min, max){
